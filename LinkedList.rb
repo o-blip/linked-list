b9479c06ff89de69f487c56c92e9ce9357b2efd6
piero <dolibiroli@gmail.com>
2022-05-02 13:59:05 -0500

    initial

---
+++ LinkedList.rb
@@ -0,0 +1,60 @@
+class LinkedList
+  def initialize
+    
+  end
+
+  def append(value)
+
+  end
+
+  def prepend(value)
+    
+  end
+
+  def size
+    
+  end
+
+  def head
+    
+  end
+
+  def tail
+    
+  end
+
+  def at(index)
+    
+  end
+
+  def pop
+    
+  end
+
+  def contains?(value)
+  end
+
+  def find(value)
+    
+  end
+
+  def to_s
+
+  end
+
+end
+
+class Node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def value(value)
+    @value = value
+  end
+
+  def next_node(next_node)
+    @next_node = next_node
+  end
+end
